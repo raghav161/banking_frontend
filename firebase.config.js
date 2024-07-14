1cb4cdcd1b0327a980e4cd0cf58306a9c67f8b22
raghav161 <raghavbansal242003@gmail.com>
2024-07-14 15:56:57 +0530

    changes in firebae

--- firebase.config.js
+++ firebase.config.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtQ35xRivBhVeW66ibSgTgd-N2o6OGsrA",
-  authDomain: "bank-login-23967.firebaseapp.com",
-  projectId: "bank-login-23967",
-  storageBucket: "bank-login-23967.appspot.com",
-  messagingSenderId: "735223576389",
-  appId: "1:735223576389:web:1c8d35d64bae8b63b8f02c",
-  measurementId: "G-S9CDBBGDY4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
